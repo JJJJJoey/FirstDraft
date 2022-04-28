dc65af2a49099d087b774886b0aa8275d7366b86
JJJJJoey <92551036+JJJJJoey@users.noreply.github.com>
2022-04-29 00:47:36 +0300

    recycler view and card view

--- app/src/main/java/gr/cityu/firstdraft/AccountActivity.java
+++ app/src/main/java/gr/cityu/firstdraft/AccountActivity.java
@@ -3,7 +3,7 @@ package gr.cityu.firstdraft;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,7 +14,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AccountActivity extends AppCompatActivity {
-    Button mButtonAccountSettings;
+    ImageView mButtonAccountSettings;
 
     private RecyclerView mRecyclerView;
     private ImageAdapter mAdapter;
